9d39e8969eeb81cec5d4f142a885b0c060f18b32
root <root@stu0054.keble.ox.ac.uk>
2012-08-13 22:32:57 +0100

    changes for readme and word.php

--- word.php
+++ word.php
@@ -1,4 +1,9 @@
 <?php
+    /*
+        Takes a url of the form "http://place.tld/path/to/word.php?words=these are words" then returns the google images result page for "these are words".
+        Used for trying to get an old flash project (which relied on another such PHP script) to work.
+    */
+
     $words = $_GET["words"];
 
     //replaces all spaces with %20 to appease google, also deals with multiple contiguous space characters
